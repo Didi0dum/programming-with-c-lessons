97a24d4c7ba85b1da8c0a7676461702b6c9f72f6
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2024-04-24 17:33:56 +0300

    Linked list lesson

---
+++ 21-linked-list/9v/tests.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "linkedlist.h"
+
+void printLinkedList(LinkedList * list) {
+  struct ListNode * currentNode = list->head;
+  while (currentNode != NULL) {
+    printf("%lf ", currentNode->value);
+    currentNode = currentNode->next;
+  }
+  printf("\n");
+}
+
+int main(void) {
+
+  printf("Executing tests: \n");
+  LinkedList list = init();
+  printf("Is head NULL: %d\n", list.head == NULL);
+  printf("Size: %u\n", list.size);
+
+  pushFront(&list, 10);
+  pushFront(&list, 15);
+  pushFront(&list, 20);
+  printLinkedList(&list);
+
+  struct ListNode * secondNode = getElement(&list, 1);
+  printf("Second value : %lf\n", secondNode->value);
+
+  push(&list, 1, 99);
+  printLinkedList(&list);
+
+  pushBack(&list, 105);
+  printLinkedList(&list);
+
+  setElement(&list, 2, 999);
+  printLinkedList(&list);
+
+  ListType firstElement = popFront(&list);
+  printf("First element: %lf \n", firstElement);
+  printLinkedList(&list);
+
+  ListType thirdElement = pop(&list, 2);
+  printf("Third element: %lf \n", thirdElement);
+  printLinkedList(&list);
+
+  ListType lastElement = popBack(&list);
+  printf("Last element: %lf \n", lastElement);
+  printLinkedList(&list);
+
+  release(&list);
+  printf("Is head NULL: %d\n", list.head == NULL);
+  printf("Size: %u\n", list.size);
+
+  return 0;
+}
